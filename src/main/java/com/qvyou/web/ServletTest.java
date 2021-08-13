e06ab2747c4517feef4e6dab27847df04cf37ee9
Zhang <1983909571@qq.com>
2021-08-13 20:45:06 +0800

    测试2

--- src/main/java/com/qvyou/web/ServletTest.java
+++ src/main/java/com/qvyou/web/ServletTest.java
@@ -19,14 +19,15 @@ public class ServletTest extends HttpServlet {
         Gson gson = new Gson(); // 这是一个工具类，可以通过它把上一步的map转换成 字符串 或者 json
         String mapJsonString = gson.toJson(map); // 如上所述，把接收到的数据转换成字符串
         System.out.println(mapJsonString); // 打印输出这个字符串
-        /* 相应部分 */
+        /* 响应部分 */
         String JavaJson = "hello"; // 一个简单的字符串，想要返回给前端
         resp.getWriter().write(JavaJson); // 像之前说的，通过resp相应给前端
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-
+        System.out.println("get被访问啦!");
+        System.out.println("get被访问啦!");
     }
 
 }
